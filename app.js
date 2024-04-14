3b088cbb33019367592e684788d196c7b7cd721f
Madeel1234 <164627451+Madeel1234@users.noreply.github.com>
2024-04-14 16:46:14 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+//Task # 25
+//Alien Color: Imagine an alien was just short down in a game. create a variable called alien color and assign its a value 
+// of green, yellow or red.
+//write an if statement to test whether the alien's color is green. if it is, print a message that the player just earned 5 points.
+// Write one version of this program that passes the if test and another that fails (the version that fails will have no output)  
+//write one version of this program that runs the if block and another that runs the else block.
+let alien_color = "green";
+if (alien_color == "green") {
+    console.log("the player just earned 5 points.");
+}
+else {
+    console.log("The player just earned 10 points.");
+}
+export {};
+/*let alien_color ="red";
+if (alien_color =="green")
+
+    console.log("no output");
+*/
